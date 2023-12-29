ab2bf41ded93bf576acb4c3edb86851ba499d6ca
Elena Anisimova <e.anisimova@lantan.info>
2023-12-29 14:28:45 +0700

    Add index and day 1 implementation

---
+++ src/day1/Trebuchet.ts
@@ -0,0 +1,74 @@
+function trebuchet(input: string[]): string
+{
+    let digits = input.map((row: string) => Number(
+        `${getFirstDigitFromInputString(row)}${getLastDigitFromInputString(row)}`)
+    );
+
+    let sum = digits.reduce((sum, currentDigit) => sum + currentDigit, 0);
+
+    return `${sum}`;
+}
+
+function getFirstDigitFromInputString(input: string): number
+{
+    let matches = matchOverlap(input, getDigitsRegexp());
+
+    if (matches && matches.length > 0) {
+        return mapNumberFromLetteredDigit(matches[0]);
+    }
+
+    return 0;
+}
+
+function getLastDigitFromInputString(input: string): number
+{
+    let matches = matchOverlap(input, getDigitsRegexp());
+
+    if (matches && matches.length > 0) {
+        return mapNumberFromLetteredDigit(matches[matches.length - 1]);
+    }
+
+    return 0;
+}
+
+function getDigitsRegexp(): RegExp
+{
+    return /([1-9]|one|two|three|four|five|six|seven|eight|nine)/gi;
+}
+
+function matchOverlap(input: string, re: RegExp) {
+    let r = [], m;
+    // Prevent infinite loops
+    if (!re.global) re = new RegExp(
+        re.source, (re+'').split('/').pop() + 'g'
+    );
+    while (m = re.exec(input)) {
+        re.lastIndex -= m[0].length - 1;
+        r.push(m[0]);
+    }
+    return r;
+}
+
+function mapNumberFromLetteredDigit(digit: string): number
+{
+    return {
+        '1': 1,
+        'one': 1,
+        '2': 2,
+        'two': 2,
+        '3': 3,
+        'three': 3,
+        '4': 4,
+        'four': 4,
+        '5': 5,
+        'five': 5,
+        '6': 6,
+        'six': 6,
+        '7': 7,
+        'seven': 7,
+        '8': 8,
+        'eight': 8,
+        '9': 9,
+        'nine': 9,
+    }[digit] ?? 0;
+}
---
+++ src/lib/InputOutput.ts
@@ -0,0 +1,20 @@
+function readInput(id: string): string[]
+{
+    let input = document.getElementById(id) as HTMLInputElement;
+
+    if (input) {
+        let rawInputValue = input.value;
+
+        return rawInputValue.split("\n");
+    }
+
+    return [];
+}
+
+function writeOutput(id: string, value: string) {
+    let output = document.getElementById(id) as HTMLInputElement;
+
+    if (output) {
+        output.value = value;
+    }
+}
